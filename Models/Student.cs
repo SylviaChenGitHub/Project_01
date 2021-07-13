e5ac124991b50c99229d861d89b406a25919eadf
sylvia <ex.workuse@gmail.com>
2021-07-14 00:05:23 +0800

    欄位驗證。

--- Models/Student.cs
+++ Models/Student.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 #nullable disable
 
@@ -8,12 +10,40 @@ namespace Project_01.Models
     public partial class Student
     {
         public string StuId { get; set; }
+        [DisplayName("中文姓名")]
+        [StringLength(20)]
         public string StuNameC { get; set; }
+        [DisplayName("英文姓名")]
+        [StringLength(50)]
         public string StuNameE { get; set; }
+        [DisplayName("身分證字號")]
+        [StringLength(10)]
+        [Required(ErrorMessage = "必填")]
         public string StuIdno { get; set; }
+        [DisplayName("電子信箱")]
+        [StringLength(50)]
+        [DataType(DataType.EmailAddress, ErrorMessage = "請輸入有效的電子信箱")]
         public string StuEmail { get; set; }
+        [DisplayName("性別")]
+        [StringLength(1)]
         public string StuGender { get; set; }
-        public DateTime? StuBirthday { get; set; }
+        [DisplayName("出生日期")]
+        [StringLength(10)]
+        [DataType(DataType.Date)]
+        public string StuBirthday { get; set; }
+        [DisplayName("入學年份")]
+        [StringLength(4)]
+        [Required(ErrorMessage = "必填")]
         public string StuYear { get; set; }
+
+        public enum Gender
+        {
+            [Display(Name = "男")]
+            M,
+            [Display(Name = "女")]
+            F,
+            [Display(Name = "其他")]
+            O
+        }
     }
 }
